a496516c2ba972c86b3e6b4733e84993aa4d396e
msseleznev <mikestm8979@gmail.com>
2022-08-08 16:20:09 +0400

    Contact page: added create contact feature

--- src/ui/pages/Contacts/Contacts.tsx
+++ src/ui/pages/Contacts/Contacts.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { getContactsTC } from '../../../bll/contacts/contacts-reducer';
+import { v1 } from 'uuid';
+
+import { createContactTC, getContactsTC } from '../../../bll/contacts/contacts-reducer';
 import { useAppDispatch, useAppSelector } from '../../../hooks/hooks';
+import { Button } from '../../common/Button/Button';
+import { InputText } from '../../common/InputText/InputText';
+import Modal from '../../common/Modal/Modal';
 import { Search } from '../../common/Search/Search';
 import { Skeleton } from '../../common/Skeleton/Skeleton';
 import { SortItem } from '../../common/SortItem/SortItem';
@@ -14,6 +19,27 @@ export const Contacts: React.FC = () => {
   const isAppFetching = useAppSelector(state => state.app.isAppFetching);
   const dispatch = useAppDispatch();
 
+  const [modalActive, setModalActive] = useState<boolean>(false);
+  const [contactId, setContactId] = useState('');
+  const [contactName, setContactName] = useState('');
+  const [contactPhone, setContactPhone] = useState('');
+
+  const createPackHandler = (): void => {
+    if (!!contactName && !!contactPhone) {
+      dispatch(
+        createContactTC({ id: contactId, name: contactName, phone: contactPhone }),
+      );
+    }
+    setContactName('');
+    setContactPhone('');
+    setModalActive(false);
+  };
+
+  const addContactHandler = (): void => {
+    setModalActive(true);
+    setContactId(v1());
+  };
+
   useEffect(() => {
     dispatch(getContactsTC());
   }, []);
@@ -22,7 +48,12 @@ export const Contacts: React.FC = () => {
     <div className={style.content}>
       <div className={style.inputBlock}>
         <Search />
-        <input className={style.button} type="submit" value="Add contact" />
+        <input
+          className={style.button}
+          type="button"
+          value="Add contact"
+          onClick={addContactHandler}
+        />
       </div>
 
       <div className={style.tableBlock}>
@@ -46,6 +77,15 @@ export const Contacts: React.FC = () => {
           </tbody>
         </table>
       </div>
+      <Modal active={modalActive} setActive={setModalActive}>
+        <h3>Name</h3>
+        <InputText value={contactName} onChangeText={setContactName} />
+        <h3>Phone number</h3>
+        <InputText value={contactPhone} onChangeText={setContactPhone} />
+        <Button style={{ marginTop: 20 }} onClick={createPackHandler}>
+          Create contact
+        </Button>
+      </Modal>
     </div>
   );
 };
